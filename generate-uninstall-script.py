9097ba6e2bfadd13bf19905392abf8882f8e148f
Kael <kaelanreece@gmail.com>
2021-04-04 17:47:11 +1000

    rewrote script to fix issues with not writinig for some files. should now better work with relative paths.

--- generate-uninstall-script.py
+++ generate-uninstall-script.py
@@ -1,8 +1,9 @@
 import os
 
-# get user input to determine the root path for the uninstall script.
+# get the root path.
 rootPath = input("Enter Path: ")
 
+# bash or powershell
 scriptType = ""
 while(scriptType != "bash" and scriptType != "powershell") :
 	scriptType = input("bash or powershell: ")
@@ -10,19 +11,16 @@ while(scriptType != "bash" and scriptType != "powershell") :
 extension = ".ps1" if scriptType == "powershell" else ".sh"
 removeCommand = "Remove-Item " if scriptType == "powershell" else "rm "
 
-# create the uninstaller in the root path of the project.
+# create the uninstaller script in the root path of the project.
 uninstallPath = rootPath + "/uninstall" + extension
 f= open(uninstallPath,"w+")
 
-for roots,dirs,files in os.walk(rootPath):
-	if(files and dirs) :
-		fullPath = ""
-		# join the directory paths together.
-		for dir in dirs:
-			fullPath = dir + "/"
-		for file in files:
-			# we want to write a removal for each file.
-			print(fullPath + file)
-			f.write(removeCommand + fullPath + file + "\n")
+for subdir,dirs,files in os.walk(rootPath):
+	if(".git" in subdir) :
+		continue
+	for file in files:
+		relPath = os.path.relpath(os.path.join(subdir, file), rootPath)
+		print(relPath)
+		f.write(removeCommand + "\"" + relPath + "\"" + "\n")
 f.close()
 print("generated uninstall script to: " + uninstallPath)
